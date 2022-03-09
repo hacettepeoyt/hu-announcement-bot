9df0dd70b68cc70ae79db7f784eb7147aef5d876
furkansimsekli <simseklifurkan0@gmail.com>
2022-03-09 03:39:39 +0300

    new functionalities added + optimized

--- bot.py
+++ bot.py
@@ -16,7 +16,10 @@ logger = logging.getLogger(__name__)
 math_website = scraper.Math()
 sksdb_website = scraper.Sksdb()
 
-websites = [sksdb_website, math_website]
+websites = {
+    'Math': math_website,
+    'SKSDB': sksdb_website
+}
 
 
 def start(update: Update, context: CallbackContext):
@@ -26,7 +29,7 @@ def start(update: Update, context: CallbackContext):
 
     update.message.reply_text(f"Hi {user_name}! I'm not fully live yet, coming soon!")
 
-    for website in websites:
+    for website in websites.values():
         website.add_subscriber(user['id'])
 
 
@@ -34,14 +37,11 @@ def isOnline(update: Update, context: CallbackContext):
     user = update.message.from_user
     user_id = user['id']
 
-    context.bot.send_message(chat_id=user_id, text="*Yes, I'm still alive*\n"
-                                                   "[Click here](www.cs.hacettepe.edu.tr)\.",
-                             parse_mode=telegram.ParseMode.MARKDOWN_V2,
-                             disable_web_page_preview=True)
+    context.bot.send_message(chat_id=user_id, text="Yes, I'm alive!")
 
 
 def check_newAnnouncements(context: CallbackContext):
-    for website in websites:
+    for website in websites.values():
         announcement = website.get_announcement()
 
         if announcement is not None:
@@ -52,11 +52,15 @@ def check_newAnnouncements(context: CallbackContext):
 def remove_subscribtion(update: Update, context: CallbackContext):
     websiteName = context.args[0]
     user_id = update.message.from_user['id']
+    website = websites[websiteName]
+    website.remove_subscriber(user_id)
 
-    if websiteName == math_website.name:
-        math_website.remove_subscriber(user_id)
-    if websiteName == sksdb_website.name:
-        sksdb_website.remove_subscriber(user_id)
+
+def add_subscribtion(update: Update, context: CallbackContext):
+    websiteName = context.args[0]
+    user_id = update.message.from_user['id']
+    website = websites[websiteName]
+    website.add_subscriber(user_id)
 
 
 def send_message(context: CallbackContext, announcement, userList):
@@ -66,11 +70,10 @@ def send_message(context: CallbackContext, announcement, userList):
 
     for user in userList:
         context.bot.send_message(chat_id=user, text=f"\U0001F514 <b>{title}</b>\n\n"
-                                                               f"\U0001F4AC {content}\n\n"
-                                                               f'\U0001F310 <a href="{url}">Click here!</a>',
+                                                    f"\U0001F4AC {content}\n\n"
+                                                    f'\U0001F310 <a href="{url}">Click here!</a>',
                                  parse_mode=telegram.ParseMode.HTML,
                                  disable_web_page_preview=True)
-
 
 
 def main():
@@ -80,6 +83,7 @@ def main():
     dispatcher.add_handler(CommandHandler("start", start)),
     dispatcher.add_handler(CommandHandler("online_status", isOnline))
     dispatcher.add_handler(CommandHandler("remove", remove_subscribtion))
+    dispatcher.add_handler(CommandHandler("add", add_subscribtion))
     updater.job_queue.run_repeating(check_newAnnouncements, interval=20, first=1)
 
     updater.start_polling()
