f8dafc7d06570f30c143aed6fdaee341824a311f
furkansimsekli <simseklifurkan0@gmail.com>
2022-03-17 17:39:50 +0300

    cs scraper updated, got rid off selenium

--- src/scraper/cs.py
+++ src/scraper/cs.py
@@ -1,9 +1,5 @@
-import json
-
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
-from database import UserDatabase
+import requests
+from bs4 import BeautifulSoup
 
 
 
@@ -18,29 +14,15 @@ class ComputerScience:
 
     def get_announcement(self):
 
-        browser = webdriver.Chrome(ChromeDriverManager().install())
-        browser.get("http://cs.hacettepe.edu.tr/#announcements")
+        r = requests.get('http://cs.hacettepe.edu.tr/json/announcements.json')
+        announcements = r.json()
+        last_announcement = announcements[0]
 
+        title = last_announcement['title']
+        body = BeautifulSoup(last_announcement['body'], 'lxml')
+        content = str(body.find('p')).replace('<p>', '').replace('</p>', '')
+        url = None
 
-        # For the title
-        # I found these ugly PATHs with Firefox xPath Finder add-on.
-        titlePath = '/html/body/div[4]/div/div[2]/div/div/div/div[1]/div/div[2]/div/table/tbody/tr[1]/td[2]'
-        title = browser.find_element(By.XPATH, titlePath).text
-
-
-        # For the content and url
-        button1 = '/html/body/div[4]/div/div[2]/div/div/div/div[1]/div/div[2]/div/table/tbody/tr[1]/td[1]'
-        contentPath = '/html/body/div[4]/div/div[2]/div/div/div/div[1]/div/div[2]/div/table/tbody/tr[2]/td/div/p'
-        urlPath = contentPath + '/a'
-
-        browser.find_element(By.XPATH, button1).click()
-        content = browser.find_element(By.XPATH, contentPath).text
-        url = browser.find_element(By.XPATH, urlPath).get_attribute('href')
-
-        self.announcement = {
-            "title": title,
-            "content": content,
-            "url": url
-        }
+        self.announcement = {'title': title, 'content': content, 'url': url}
 
         return self.announcement
