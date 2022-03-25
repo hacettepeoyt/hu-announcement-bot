91228c568c91be61c0122f9c6529cdb6888fe816
furkansimsekli <simseklifurkan0@gmail.com>
2022-03-25 21:44:53 +0300

    updated format

--- src/scraper/cs.py
+++ src/scraper/cs.py
@@ -12,16 +12,28 @@ class ComputerScience:
         self.name = 'Computer Science'
 
 
+    @staticmethod
+    def __complete_url(text):
+
+        if text[:4] == 'http' or text[:3] == 'www':
+            url = text
+        else:
+            url = 'http://www.cs.hacettepe.edu.tr' + text
+
+        return url
+
+
     def get_announcement(self):
 
         r = requests.get('http://cs.hacettepe.edu.tr/json/announcements.json')
+
         announcements = r.json()
         last_announcement = announcements[0]
+        body = BeautifulSoup(last_announcement['body'], 'lxml')
 
         title = last_announcement['title']
-        body = BeautifulSoup(last_announcement['body'], 'lxml')
-        content = str(body.find('p')).replace('<p>', '').replace('</p>', '')
-        url = None
+        content = body.find('p').text
+        url = self.__complete_url(body.find('a').get('href'))
 
         self.announcement = {'title': title, 'content': content, 'url': url}
 
