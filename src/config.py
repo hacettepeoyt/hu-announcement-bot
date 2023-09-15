b2c7a874ea3635b69d06b1a2ad83c2fcb141237e
furkansimsekli <simseklifurkan0@gmail.com>
2023-09-15 21:43:12 +0300

    config: integrate toml
    Resolves #36

--- src/config.py
+++ src/config.py
@@ -1,26 +1,33 @@
+import sys
+
+import toml
+
+config_path = sys.argv[1] if len(sys.argv) > 1 else "config.toml"
+config = toml.load(config_path)
+
 # Telegram Bot Token
-TELEGRAM_API_KEY: str = ''
+TELEGRAM_API_KEY: str = config["TELEGRAM_API_KEY"]
 
 # MongoDB Connection String
-DB_STRING: str = ''
+DB_STRING: str = config["DB_STRING"]
 
 # Feedback Chat ID
-FEEDBACK_CHAT_ID: int = 0
+FEEDBACK_CHAT_ID: int = config["FEEDBACK_CHAT_ID"]
 
 # Admin Chat ID
-ADMIN_ID: int = 0
+ADMIN_ID: int = config["ADMIN_ID"]
 
 # Logger Chat ID
-LOGGER_CHAT_ID: int = 0
+LOGGER_CHAT_ID: int = config["LOGGER_CHAT_ID"]
 
 # Default Department List
-DEFAULT_DEPS: list[str] = []
+DEFAULT_DEPS: list[str] = config["DEFAULT_DEPS"]
 
 # Time Configurations for the Announcement Checking Task (in seconds)
-ANNOUNCEMENT_CHECK_INTERVAL: int = 0
-ANNOUNCEMENT_CHECK_FIRST: int = 0
+ANNOUNCEMENT_CHECK_INTERVAL: int = config["ANNOUNCEMENT_CHECK_INTERVAL"]
+ANNOUNCEMENT_CHECK_FIRST: int = config["ANNOUNCEMENT_CHECK_FIRST"]
 
 # Polling or Webhook?
-WEBHOOK_CONNECTED: bool = False
-PORT: str = ''
-WEBHOOK_URL: str = '' + TELEGRAM_API_KEY
+WEBHOOK_CONNECTED: bool = config["WEBHOOK_CONNECTED"]
+PORT: str = config["PORT"]
+WEBHOOK_URL: str = config["WEBHOOK_URL"]
